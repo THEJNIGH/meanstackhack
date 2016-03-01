ff19ceffdf3094bdf4377bd6f2d885679a922509
therealaldo <agonzalez2@fullsail.edu>
2016-03-01 18:45:33 -0500

    added route for home page

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Routes
-app.use('/', require('../routes/home.js')(express));
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/views/index.html');
+})
 
 const server = app.listen(port, function() {
   console.log('Listening on port ' + port + '...');
